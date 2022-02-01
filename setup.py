b4e50bdc25f329dc3472f1998312ddb765cb6747
Yuri Silveira <ypereirars@gmail.com>
2022-01-31 21:52:24 -0300

    add setup configuration

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='progress-bar',
+   version='0.0.1',
+   description='A progress bar for training models',
+   author='Yuri Silveira',
+   author_email='ypereirars@gmail.com',
+   packages=['progress-bar']
+)
