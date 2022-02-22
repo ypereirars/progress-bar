f8111942cfa56fe0eb55303d3806134d23349dbf
Yuri Silveira <ypereirars@gmail.com>
2022-02-22 20:13:42 -0300

    fix package name

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-   name='progress-bar',
+   name='progress_bar',
    version='0.0.1',
    description='A simple progress bar for training models',
    author='Yuri Silveira',
    author_email='ypereirars@gmail.com',
-   packages=['progress-bar']
+   packages=['progress_bar']
 )
